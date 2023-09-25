791de31fe9018c29621cf361e3272f24b1c59d4d
askar <kylymbaevaskar12@gmail.com>
2023-09-25 19:16:25 +0600

    added text

--- ad.js
+++ ad.js
@@ -1,1 +1,20 @@
-document
+let input1 = document.querySelector(".input-1");
+let btn = document.querySelector(".btn");
+let out = document.querySelector(".out");
+let input2 = document.querySelector(".input-2") ;
+let input3 = document.querySelector(".input-3") ;
+let input4 = document.querySelector(".input-4") ;
+
+function sendMassege() {
+    out.innerHTML = input1.value;
+    input2.value = input1.value;
+    input3.value = input1.value;
+    input4.value = input1.value;
+    
+
+}
+
+
+
+
+
